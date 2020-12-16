31f21d15e402ccc157cea71af9842e76a9734d7a
qaweey Soetan <qaweey.soetan@thebulb.africa>
2020-12-16 10:55:52 +0100

    Add project files.

---
+++ StreamAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StreamAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your file name");
+           string name= Console.ReadLine();
+            string path = "C:\\Users\\QaweeySoetan\\Documents\\bidex\\" + name  +".html"; 
+             
+            if (!File.Exists(path))
+            {
+                
+                 Console.WriteLine("Please write the topic you want to talk about ");
+             string topic=Console.ReadLine();
+            File.AppendAllText(path, "<h1>" + topic + "<h1>");
+            Console.WriteLine("How many paragraphs of content  do you want to write");
+           int n =int.Parse(Console.ReadLine());
+
+
+                
+
+                int count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    count++;
+                    Console.WriteLine("Paragraph"+count);
+                string  content =  Console.ReadLine()  ;
+                    string me = "<h4>" + content + "</h4>";
+
+                    var list = new List<string>() { me };
+                    File.AppendAllLines(path, list);
+                    
+
+
+                }
+               
+                Console.WriteLine("Thank you");
+                
+            }
+           ;
+            
+
+        }
+    }
+}
---
+++ assignment2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path= "C:\\Users\\QaweeySoetan\\Documents\\bidex\\.txt";
+
+            int count = 0;
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < 3; i++)
+               
+            {
+                count++;
+                Console.WriteLine("Please write activity"+count );
+                string topic = Console.ReadLine();
+               
+                list.Add(topic);
+
+
+                
+
+
+
+
+            }
+            list.Reverse();
+            File.AppendAllLines(path, list);
+        }
+    }
+}
